6b6b5ec01b320892fabd22ca693f6caa64b12634
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-02-08 13:26:42 -0500

    ...

--- test/plotstream.lua
+++ test/plotstream.lua
@@ -6,10 +6,10 @@ h5_open_file(cmdline.args[1], "r")
 
 visual.open_window()
 
-for i=0,900,10 do
+for i=0,1850,10 do
    local S = h5_read_array(string.format("mag%04d", i))
    visual.draw_lines3d(S)
-   os.execute('sleep 0.01')
+   os.execute('sleep 0.1')
 end
 
 
